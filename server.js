c938ebb694747c1b8f8c5d4c60e959a625d94262
Zubin Dodhi <zubindodhi@Zubins-MacBook-Pro.local>
2018-01-29 11:28:19 -1000

    GET method

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+
+
+
 http = require('http');
 const fs = require('fs');
 //create  server
 const PORT = process.env.PORT || 8080;
 const server = http.createServer((request, response) => {
-  const {headers,method,url} = request;
+  let method = request.method;
+  let url = request.url;
 
 
-  // console.log('public: ', public);
+  
   console.log('method:', request.method);
   console.log('version:', request.httpVersion);
   console.log('url:', url);
-  // console.log('body: ', body);
-  // console.log('responseBody: ', responseBody);
+  
+  
 
   switch (method) {
 
@@ -22,23 +26,41 @@ const server = http.createServer((request, response) => {
     else if (url) {
       getUrl(request, response, url);
     }
-    
     break;
+    case "POST":
+    if (url){
+      postData(request,response);
+    }
     default:
-    response.writeHead(404, 'Not Found');
-    response.end();
+    err404 (request, response)
+    break;
   }
 
   
 
 });
+// server listening to PORT
 server.listen(PORT, () => {
     console.log(`Server listening port: ${PORT}`);
   });
-function getIndex (request, response) {
-  fs.readFile("./public/index.html", function (err, data) {
+  // Returns index in new public file
+function getIndex (request, response,) {
+  fs.readFile('./public/index.html', function (err, data) {
     if (err) {
-      throw err;
+       err404(request,response);
+    } 
+    else {
+      response.writeHead(200, "OK");
+      response.write(data);
+      response.end();
+    }
+  });
+}
+// get css/other
+function getUrl (request, response, url) {
+  fs.readFile('./public' + url, function(err, data){
+    if (err){
+       err404(request,response);
     }
     else {
       response.writeHead(200, "OK");
@@ -47,20 +69,45 @@ function getIndex (request, response) {
     }
   });
 }
-
-function getUrl (request, response, url) {
-  fs.readFile('./public' + url, function(err, data){
+//error 404.html
+function err404 (request, response) {
+  fs.readFile('./public/404.html', function(err, data){
     if (err){
       throw err;
     }
     else {
-      response.writeHead(200, "OK");
+      response.writeHead(404, "Error");
       response.write(data);
       response.end();
     }
   });
 }
 
+
+
+
+// test functions
+
+function postData(request,response) {
+  
+
+  let body = '';
+  request.on('data', function (data) {
+      body += data;
+      console.log("body: " + body);
+  });
+  request.on('end', function () {
+      console.log("Body: " + body);
+  });
+  response.writeHead(200, "OK");
+  response.end();
+}
+
+// function err405 (request, response) {
+//   response.writeHead(405, "Method Not Supported");
+//         response.write("<html><body>405: Method not supported. Go to <a href='/index.html'>homepage</a></body></html>");
+//         response.end();
+// }
 
 
 
